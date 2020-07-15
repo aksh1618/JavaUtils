fe78c9068b24846eaf00a811d8f7595c4c170dab
aksh1618 <aakarshituppal@gmail.com>
2020-07-16 00:48:30 +0530

    Improved Matcher ⬆

--- Matcher.java
+++ Matcher.java
@@ -2,7 +2,9 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
@@ -14,6 +16,7 @@ import java.util.function.Supplier;
  *      matches(myVal, () -> "Matched myVal!"),
  *      instanceOf(MyClass.class, () -> "Instance of MyClass!"),
  *      satisfies(val -> val > 10 && val < 15, () -> "Between 10 & 15!"),
+ *      otherwise(() -> "¯\_(ツ)_/¯")
  *  );
  * }</pre>
  *
@@ -25,26 +28,45 @@ import java.util.function.Supplier;
 public class Matcher<T, U> {
 
     private final Predicate<T> predicate;
-    private final Supplier<U> result;
+    private final Supplier<U> resultSupplier;
 
+    /**
+     * @see Matcher#when(Object, Matcher[])
+     */
+    @SafeVarargs
+    public static <T, U> Function<T, Optional<U>> whenValue(Matcher<T, U>... matchers) {
+        return value -> when(value, matchers);
+    }
+
+    /**
+     * @see Matcher
+     */
     @SafeVarargs
     public static <T, U> Optional<U> when(T value, Matcher<T, U>... matchers) {
         return Arrays.stream(matchers)
                 .filter(matcher -> matcher.predicate.test(value))
                 .findFirst()
-                .map(matcher -> matcher.result.get());
+                .map(matcher -> matcher.resultSupplier.get());
     }
 
-    public static <T, U> Matcher<T, U> matches(T value, Supplier<U> result) {
-        return new Matcher<>(value::equals, result);
+    public static <T, U> Matcher<T, U> matches(T value, Supplier<U> resultSupplier) {
+        return new Matcher<>(value::equals, resultSupplier);
     }
 
-    public static <T, U, V> Matcher<T, U> instanceOf(Class<V> clazz, Supplier<U> result) {
-        return new Matcher<>(clazz::isInstance, result);
+    public static <T, U> Matcher<T, U> matchesAny(List<T> values, Supplier<U> resultSupplier) {
+        return new Matcher<>(t -> values.stream().anyMatch(v -> v.equals(t)), resultSupplier);
     }
 
-    public static <T, U> Matcher<T, U> satisfies(Predicate<T> predicate, Supplier<U> result) {
-        return new Matcher<>(predicate, result);
+    public static <T, U, V> Matcher<T, U> instanceOf(Class<V> clazz, Supplier<U> resultSupplier) {
+        return new Matcher<>(clazz::isInstance, resultSupplier);
+    }
+
+    public static <T, U> Matcher<T, U> satisfies(Predicate<T> predicate, Supplier<U> resultSupplier) {
+        return new Matcher<>(predicate, resultSupplier);
+    }
+
+    public static <T, U> Matcher<T, U> otherwise(Supplier<U> resultSupplier) {
+        return new Matcher<>(__ -> true, resultSupplier);
     }
 
 }
